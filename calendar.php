365b44a1f150d76925852eae20a7ba0ba16955ff
Karim Shobak <karimshobak@gmail.com>
2026-02-01 16:00:36 +0200

    fix bug (when user is not logged system will direct him to login page automatically)

--- calendar.php
+++ calendar.php
@@ -1,8 +1,11 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
+    session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: auth/login.php");
+    exit;
+}
 
 require 'connection.php';
 
